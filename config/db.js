35ee11d59fdffcd1a09a190067d9592cd5bbd33e
Santhosh-R1 <santhoshrajan817@gmail.com>
2026-06-09 15:25:30 +0530

    push

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+// Explicitly use Google/Cloudflare DNS to bypass local ISP DNS blocking of SRV records
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
